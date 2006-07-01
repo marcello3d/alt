cf56a3eb955f71a3f94df39b5e2c795ccc88e756
Marcello <Marcello@cfcd7bd4-8716-0410-837e-d794d42be362>
2006-07-01 22:57:13 +0000

    Rearranged libraries

--- src/cello/alt/launcher/Launcher.java
+++ src/cello/alt/launcher/Launcher.java
@@ -2,10 +2,8 @@ package cello.alt.launcher;
 
 import java.io.File;
 import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.HashSet;
-import java.util.Set;
+
+import cello.alt.util.DynamicJarClassLoader;
 
 /**
  * A dynamic class loader (based off helma.main.launcher from http://helma.org/)
@@ -23,10 +21,14 @@ public class Launcher {
         try {
             // Get base directory
             File base = new File(System.getProperty("alt.home","."));
-            File libdir = new File(base, "lib");
+            File libdirs[] = {
+                    new File(base, "lib/jetty"),
+                    new File(base, "lib/core"),
+                    new File(base, "lib/ext")
+            };
             
             // Construct a loader
-            ClassLoader loader = new DynamicJarClassLoader(libdir);
+            ClassLoader loader = new DynamicJarClassLoader(libdirs);
             Thread.currentThread().setContextClassLoader(loader);
             
             // Initialize HTTP server
@@ -39,72 +41,6 @@ public class Launcher {
         } catch (Throwable t) {
             System.err.println("Cannot start cello.alt.server.HTTPServer:");
             t.printStackTrace(System.err);
-        }
-    }
-    
-    private static class DynamicJarClassLoader extends URLClassLoader {
-        private File base; 
-        private Set<File> seenFiles;
-        /**
-         * Constructs a new DynamicClassLoader with a particular folder to 
-         *  search for jar/zip files within.
-         * @param base
-         */
-        public DynamicJarClassLoader(File base) {
-            super(new URL[]{});
-            this.base = base;
-            this.seenFiles = new HashSet<File>();
-            checkFolder();
-        }
-        /**
-         * Just overridden for debugging...
-         * @param url  url to add
-         */
-        @Override
-        public void addURL(URL url) {
-            super.addURL(url);
-            System.out.println("Adding URL... "+url);
-        }
-        /**
-         * If we have trouble finding a class, check the if the folder has been 
-         * updated and try again, otherwise fail.
-         * 
-         * @param name the name of the class
-         * @return the class
-         * @throws ClassNotFoundException if the class was not found
-         */
-        @Override
-        public Class<?> findClass(String name) throws ClassNotFoundException {
-            try {
-                return super.findClass(name);
-            } catch (ClassNotFoundException ex) {
-                if (checkFolder())
-                    return super.findClass(name);
-                throw ex;
-            }
-        }
-        /**
-         * Checks if a folder is updated
-         * @return true if the folder is updated
-         */
-        private boolean checkFolder() {
-            File[] libs = base.listFiles();
-            
-            boolean addedSomething = false;
-            for (File f : libs) 
-                if (!seenFiles.contains(f))
-                    try {
-                        seenFiles.add(f);
-                        String name = f.getName().toLowerCase();
-                        if (name.endsWith(".jar") || name.endsWith(".zip")) {
-                            addURL(new URL("file:"+f.getCanonicalPath()));
-                            addedSomething = true;
-                        }
-                    } catch (Exception ex) {
-                        // continue
-                        System.err.println("cannot add "+f+":"+ex);
-                    }
-            return addedSomething;
         }
     }
 }
---
+++ src/cello/alt/util/DynamicJarClassLoader.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package cello.alt.util;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This ClassLoader allows a directory of jars to be used as a class path.
+ * Currently this allows for dynamically loading .jar files added after the
+ * class loader is initialized, although it currently does not support reloading 
+ * of modified .jar files. 
+ * 
+ * @author Marcello
+ *
+ */
+public class DynamicJarClassLoader extends URLClassLoader {
+    private File bases[]; 
+    private Set<File> seenFiles;
+    /**
+     * Constructs a new DynamicClassLoader with a particular folder to 
+     *  search for jar/zip files within.
+     * @param bases  bases path to look for jar files
+     */
+    public DynamicJarClassLoader(File bases[]) {
+        super(new URL[]{});
+        this.bases = bases;
+        this.seenFiles = new HashSet<File>();
+        checkFolder();
+    }
+    /**
+     * Constructs a new DynamicClassLoader with a particular folder to 
+     *  search for jar/zip files within.
+     * @param bases  bases path to look for jar files
+     * @param parent parent class loader
+     */
+    
+    public DynamicJarClassLoader(File bases[], ClassLoader parent) {
+        super(new URL[]{}, parent);
+        this.bases = bases;
+        this.seenFiles = new HashSet<File>();
+        checkFolder();
+    }
+    /**
+     * Overridden to make public.
+     * @param url  url to add
+     * @see java.net.URLClassLoader#addURL(java.net.URL)
+     */
+    @Override
+    public void addURL(URL url) {
+        super.addURL(url);
+        System.out.println("Adding URL... "+url);
+    }
+    /**
+     * If we have trouble finding a class, check the if the folder has been 
+     * updated and try again, otherwise fail.
+     * 
+     * @param name the name of the class
+     * @return the class
+     * @throws ClassNotFoundException if the class was not found
+     */
+    @Override
+    public Class<?> findClass(String name) throws ClassNotFoundException {
+        try {
+            return super.findClass(name);
+        } catch (ClassNotFoundException ex) {
+            if (checkFolder())
+                return super.findClass(name);
+            throw ex;
+        }
+    }
+    /**
+     * Checks if a folder is updated
+     * @return true if the folder is updated
+     */
+    private boolean checkFolder() {
+        boolean addedSomething = false;
+        for (File base : this.bases) {
+            File[] libs = base.listFiles();
+            
+            for (File f : libs) 
+                if (!seenFiles.contains(f))
+                    try {
+                        seenFiles.add(f);
+                        String name = f.getName().toLowerCase();
+                        if (name.endsWith(".jar") || name.endsWith(".zip")) {
+                            addURL(new URL("file:"+f.getCanonicalPath()));
+                            addedSomething = true;
+                        }
+                    } catch (Exception ex) {
+                        // continue
+                        System.err.println("cannot add "+f+":"+ex);
+                    }
+        }
+        return addedSomething;
+    }
+}
