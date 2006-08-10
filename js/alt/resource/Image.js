4990763cdeea0874ba656311aea5309d08a8177e
Marcello <Marcello@cfcd7bd4-8716-0410-837e-d794d42be362>
2006-08-10 23:53:42 +0000

    Moved javascript sources to src folder

--- js/alt/resource/Image.js
+++
@@ -1,9 +0,0 @@
-
-Alt.require('alt.resource.Loader', true);
-
-function Image(resource) {
-	return Packages.javax.imageio.ImageIO.read(resource.stream);
-}
-
-for each (var format in Packages.javax.imageio.ImageIO.readerFormatNames)
-	Loader.defineType(Image, format);
