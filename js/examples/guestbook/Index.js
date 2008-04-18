e567e5697bb3fe94a3b91267fe57f124b171610b
Marcello <Marcello@cfcd7bd4-8716-0410-837e-d794d42be362>
2008-04-18 19:27:50 +0000

    cleaned up plus commented

--- js/examples/guestbook/Index.js
+++ js/examples/guestbook/Index.js
@@ -2,13 +2,13 @@ displaySource("Index.js",'GuestBook.onion.xml');
 
 Alt.require("alt.resource.XML");
 
-Alt.require("examples.guestbook.Dates")
-
 var onion = new Onion(Resources.load('GuestBook.onion.xml'));
 
+// Make the datastore if we don't have it --- just a javascript array!
 if (!examples.guestbook.posts || request.getParameter('reset'))
 	examples.guestbook.posts = [];
 
+// if there is a post, add it to the array
 if (request.method=="POST") {
 	examples.guestbook.posts.push({
 		name: request.getParameter('name'),
@@ -44,13 +44,16 @@ function whereFunc(item,args) {
 function Store(array){
 	this.store = array;
 }
+// dumb slow query function!
 Store.prototype.query = function(o) {
 	var array = this.store;
-	if (o.order || o.where) {
+	// don't touch original array if we're ordering or whereing
+	if (o.where) {
 		array = new Array();
 		
 		for (var i = 0; i < this.store.length; i++) {
 			var accept = true;
+			// check all where conditions (no OR support yet!)
 			if (o.where)
 				for (var k=0; k<o.where.length; k++)
 					if (!whereFunc(this.store[i][o.where[k][0]], o.where[k])) {
@@ -62,6 +65,7 @@ Store.prototype.query = function(o) {
 		}
 		
 	} 
+	// sort by list of sort items
 	if (o.order) {
 		array.sort(function(a,b) {
 			for (var i = 0; i < o.order.length; i++) {
@@ -75,14 +79,16 @@ Store.prototype.query = function(o) {
 			return 0;
 		});
 	} 
-	if (o.limit || o.offset) {
-		Alt.log("splice "+(o.offset || 0)+", "+((o.offset || 0) + o.limit));
+	// grab subset of results
+	if (o.limit || o.offset)
 		array = array.slice(o.offset || 0, (o.offset || 0) + o.limit);
-	}
 	return array;
 };
 Store.prototype.count = function(o) {
-	var array = this;
+	// we only care about where here (do we care about limit?)
+	var array = this.store;
+	
+	// count all items that match if there is a where clause
 	if (o.where) {
 		var count = 0;
 		for (var i = 0; i < this.store.length; i++) {
@@ -102,51 +108,56 @@ Store.prototype.count = function(o) {
 
 
 var where = false;
+// based on http get parameter show...
 switch (request.getParameter('show')) {
 	case 'all':
-		where = false;
+		// no where clause
+		where = false; 
 		break;
 	case 'domestic':
+		// where international is false
 		where = [['international','!']];
 		break;
 	case 'international':
+		// where international is true
 		where = [['international']];
 		break;		
 }
 
-var arr = [2,3,4];
-for each (var i in arr) {
-	Alt.log("arr.i="+i);
-}
+// Create a store and count everything that applies
+var store = new Store(examples.guestbook.posts);
+var count = store.count({ where: where });
 
-var count = new Store(examples.guestbook.posts).count({
-	where: where
-});
+// calculate number of pages based on that count
 var perpage = 5;
 var pages = Math.ceil(count / perpage);
 
-var page = request.getParameter('page')-1;
-if (page<0) page=0;
-if (page>=pages) page = pages-1; 
+// current page
+var page = Math.max(0,Math.min(pages-1,request.getParameter('page')-1)); 
 
-var display = new Store(examples.guestbook.posts).query({
+// find the entries
+var postsToShow = store.query({
 	where: where,
 	offset: page*perpage,
 	limit: perpage,
 	order: [['time',-1]]
 });
 
+// generate site
 var site = onion.evaluate(
 	<guestbook>
 	 <page>{page+1}</page>
 	 <pages>{pages}</pages>
 	</guestbook>, 
 	// Data
-	{ posts: display }
+	{ posts: postsToShow }
 );
 
 /*
 response.contentType = 'application/xhtml+xml';
 response.write('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">');
 */
+
+// for some reason firefox can't handle <textarea /> in html mode, 
+// and running in proper xhtml mode is currently a bitch
 response.write(site.toString().replace(/<textarea([^>]+)\/>/m,'<textarea$1></textarea>'));
