663ac6de74384ce65d485a1b64504207400def8b
Marcello Bastea-Forte <marcello@marcello-dev.local>
2011-02-02 20:15:59 -0500

    tabs -> spaces

--- src/alt-core-1.0.js
+++ src/alt-core-1.0.js
@@ -36,12 +36,12 @@
  * @return {void}
  */
 Object.forEach = function Object$forEach(object,fun,thisPointer) {
-	for (var key in object) {
-		var value = object[key];
-		if (value !== Object.prototype[key]) {
-			fun.call(thisPointer, key, value, object);
-		}
-	}
+    for (var key in object) {
+        var value = object[key];
+        if (value !== Object.prototype[key]) {
+            fun.call(thisPointer, key, value, object);
+        }
+    }
 };
 /**
  * This method copies values from a given source object to the current object.
@@ -49,10 +49,10 @@ Object.forEach = function Object$forEach(object,fun,thisPointer) {
  * @return this
  */
 Object.extend = function Object$extend(destination,source) {
-	Object.forEach(source,function(key,value) {
-		destination[key] = value;
-	});
-	return destination;
+    Object.forEach(source,function(key,value) {
+        destination[key] = value;
+    });
+    return destination;
 };
 /**
  * This method (like Object.extend) copies values from a given source object to the current
@@ -69,7 +69,7 @@ Object.extendMissing = function Object$extendMissing(destination,source) {
             }
         });
     }
-	return destination;
+    return destination;
 };
 
 /**
@@ -83,178 +83,178 @@ Object.extendMissing = function Object$extendMissing(destination,source) {
  * @return a new Array with length arrayLike.length, and values 0 through arrayLike.length-1 from arrayLike
  */
 Array.from = function Array$from(arrayLike) {
-	var array = new Array(arrayLike.length),
+    var array = new Array(arrayLike.length),
         i=0;
-	for (; i<arrayLike.length; i++) {
-		if (i in arrayLike) {
-			array[i] = arrayLike[i];
-		}
-	}
-	return array;
+    for (; i<arrayLike.length; i++) {
+        if (i in arrayLike) {
+            array[i] = arrayLike[i];
+        }
+    }
+    return array;
 };
 Object.extendMissing(Array.prototype, {
-	/**
-	 * <p><strong>JavaScript 1.6 browsers implement this method natively.  The difference between
-	 * this version and the Mozilla implementation is this version does less error checking.</strong></p>
-	 *
-	 * <p>This method loops over an array and calls the function 'fun' for each value in
-	 * the array.</p>
-	 * <p>The fun will be called with the arguments [array[index], index, array], where index
-	 * is the current position in the array.</p>
-	 * <p>An optional "thisPointer" can be passed to this method and will be passed to fun.</p>
-	 * <p>Note, this method skips undefined indices (e.g. with sparse Arrays).</p>
-	 * @param fun callback function executed for each defined index in the Array
-	 * @param [thisPointer] optional this pointer
-	 * @return {void}
-	 */
-	forEach: function Array$forEach(fun, thisPointer) {
-		for (var i = 0, length = this.length; i < length; i++) {
-			if (i in this) {
-				fun.call(thisPointer, this[i], i, this);
-			}
-		}
-	},
+    /**
+     * <p><strong>JavaScript 1.6 browsers implement this method natively.  The difference between
+     * this version and the Mozilla implementation is this version does less error checking.</strong></p>
+     *
+     * <p>This method loops over an array and calls the function 'fun' for each value in
+     * the array.</p>
+     * <p>The fun will be called with the arguments [array[index], index, array], where index
+     * is the current position in the array.</p>
+     * <p>An optional "thisPointer" can be passed to this method and will be passed to fun.</p>
+     * <p>Note, this method skips undefined indices (e.g. with sparse Arrays).</p>
+     * @param fun callback function executed for each defined index in the Array
+     * @param [thisPointer] optional this pointer
+     * @return {void}
+     */
+    forEach: function Array$forEach(fun, thisPointer) {
+        for (var i = 0, length = this.length; i < length; i++) {
+            if (i in this) {
+                fun.call(thisPointer, this[i], i, this);
+            }
+        }
+    },
 
-	/**
-	 * <p><strong>JavaScript 1.6 browsers implement this method natively.  The difference between
-	 * this version and the Mozilla implementation is this version does less error checking.</strong></p>
-	 *
-	 * <p>This method creates a new Array with values constructed by passing the values of an existing
-	 * Array into a function and storing the return values.</p>
-	 * <p>The fun will be called with the arguments [array[index], index, array], where index
-	 * is the current position in the array.</p>
-	 * <p>An optional "thisPointer" can be passed to this method and will be passed to fun.</p>
-	 * <p>Note, this method skips undefined indices (e.g. with sparse Arrays).</p>
-	 * @param fun callback function executed for each defined index in the Array
-	 * @param thisPointer optional this pointer
-	 * @return {Array} the new array
-	 */
-	map: function Array$map(fun, thisPointer) {
-		var result = new Array(length);
-		for (var i = 0, length = this.length; i < length; i++) {
-			if (i in this) {
-				result[i] = fun.call(thisPointer, this[i], i, this);
-			}
-		}
-		return result;
-	},
+    /**
+     * <p><strong>JavaScript 1.6 browsers implement this method natively.  The difference between
+     * this version and the Mozilla implementation is this version does less error checking.</strong></p>
+     *
+     * <p>This method creates a new Array with values constructed by passing the values of an existing
+     * Array into a function and storing the return values.</p>
+     * <p>The fun will be called with the arguments [array[index], index, array], where index
+     * is the current position in the array.</p>
+     * <p>An optional "thisPointer" can be passed to this method and will be passed to fun.</p>
+     * <p>Note, this method skips undefined indices (e.g. with sparse Arrays).</p>
+     * @param fun callback function executed for each defined index in the Array
+     * @param thisPointer optional this pointer
+     * @return {Array} the new array
+     */
+    map: function Array$map(fun, thisPointer) {
+        var result = new Array(length);
+        for (var i = 0, length = this.length; i < length; i++) {
+            if (i in this) {
+                result[i] = fun.call(thisPointer, this[i], i, this);
+            }
+        }
+        return result;
+    },
 
-	/**
-	 * <p><strong>JavaScript 1.6 browsers implement this method natively.  The difference between
-	 * this version and the Mozilla implementation is this version does less error checking.</strong></p>
-	 *
-	 * <p>This method creates a new Array by filtering the original array's values through a function.</p>
-	 * <p>The fun will be called with the arguments [array[index], index, array], where index
-	 * is the current position in the array.</p>
-	 * <p>An optional "thisPointer" can be passed to this method and will be passed to fun.</p>
-	 * <p>Note, this method skips undefined indices (e.g. with sparse Arrays).</p>
-	 * @param fun callback function executed for each defined index in the Array
-	 * @param thisPointer optional this pointer
-	 * @return {Array} the new array
-	 */
-	filter: function Array$filter(fun, thisPointer) {
-		var length = this.length,
-		    result = [],
+    /**
+     * <p><strong>JavaScript 1.6 browsers implement this method natively.  The difference between
+     * this version and the Mozilla implementation is this version does less error checking.</strong></p>
+     *
+     * <p>This method creates a new Array by filtering the original array's values through a function.</p>
+     * <p>The fun will be called with the arguments [array[index], index, array], where index
+     * is the current position in the array.</p>
+     * <p>An optional "thisPointer" can be passed to this method and will be passed to fun.</p>
+     * <p>Note, this method skips undefined indices (e.g. with sparse Arrays).</p>
+     * @param fun callback function executed for each defined index in the Array
+     * @param thisPointer optional this pointer
+     * @return {Array} the new array
+     */
+    filter: function Array$filter(fun, thisPointer) {
+        var length = this.length,
+            result = [],
             i=0,
             val;
-		for (; i < length; i++) {
-			if (i in this) {
-				val = this[i]; // in case fun mutates this
-				if (fun.call(thisPointer, val, i, this)) {
-					result.push(val);
-				}
-			}
-		}
-		return result;
-	}
+        for (; i < length; i++) {
+            if (i in this) {
+                val = this[i]; // in case fun mutates this
+                if (fun.call(thisPointer, val, i, this)) {
+                    result.push(val);
+                }
+            }
+        }
+        return result;
+    }
 });
 
 Object.extend(Function.prototype, {
-	/**
-	 * Create a subclass of an existing class with given (optional) methods.
-	 * Class Methods (functions on an object) can be specified through the methods argument.
-	 * You can specify a constructor by defining a method named "constructor."  Otherwise
-	 * the default constructor will be used.  The default constructor calls the parent
-	 * constructor:
-	 * <pre>
-	 * function Function$DefaultConstructor() {
-	 *		arguments.callee.parent.call(this);
-	 *	}
-	 * </pre>
-	 * Example:
-	 * <pre>
-	 * var A = Object.subclass({
-	 * 	constructor: function(name) {
-	 * 		log("A constructor("+name+")");
-	 * 		this.name = name;
-	 * 	},
-	 * 	foo: function() {
-	 * 		log("A("+this.name+").foo()");
-	 * 	},
-	 * 	bar: function() {
-	 * 		log("A("+this.name+").bar()");
-	 * 	}
-	 * });
+    /**
+     * Create a subclass of an existing class with given (optional) methods.
+     * Class Methods (functions on an object) can be specified through the methods argument.
+     * You can specify a constructor by defining a method named "constructor."  Otherwise
+     * the default constructor will be used.  The default constructor calls the parent
+     * constructor:
+     * <pre>
+     * function Function$DefaultConstructor() {
+     *        arguments.callee.parent.call(this);
+     *    }
+     * </pre>
+     * Example:
+     * <pre>
+     * var A = Object.subclass({
+     *     constructor: function(name) {
+     *         log("A constructor("+name+")");
+     *         this.name = name;
+     *     },
+     *     foo: function() {
+     *         log("A("+this.name+").foo()");
+     *     },
+     *     bar: function() {
+     *         log("A("+this.name+").bar()");
+     *     }
+     * });
      *
-	 * var B = A.subclass({
-	 * 	constructor: function(name,arg2) {
-	 *      // call super constructor
-	 * 		arguments.callee.parent.call(this,name);
-     * 		log("B constructor("+name+","+arg2+")");
-	 * 	},
-	 * 	foo: function() {
-	 *      // call super class "foo" method
-	 * 		arguments.callee.parent.call(this);
-	 * 		log("B("+this.name+").foo()");
-	 * 	}
-	 * });
-	 * </pre>
-	 *
-	 * @see Function.prototype.addMethods
-	 * @param [methods] an associative Object of method names -> functions
-	 * @return the new class
-	 */
-	subclass: function Function$subclass(methods) {
-		var constructor = (methods && methods.constructor !== Object && methods.constructor) ||
-			(this !== Object &&
-			function Function$DefaultSubclassConstructor() {
-				arguments.callee.parent.call(this);
-			}) ||
-			function Function$ObjectSubclassConstructor() {};
-		constructor.parent = this;
-		constructor.prototype = new this;
-		if (methods) {
-			constructor.addMethods(methods);
-		}
-		constructor.prototype.constructor = constructor;
-		return constructor;
-	},
-	addMethods: function Function$addMethods(methods) {
-		Object.forEach(methods,function(name,method) {
+     * var B = A.subclass({
+     *     constructor: function(name,arg2) {
+     *      // call super constructor
+     *         arguments.callee.parent.call(this,name);
+     *         log("B constructor("+name+","+arg2+")");
+     *     },
+     *     foo: function() {
+     *      // call super class "foo" method
+     *         arguments.callee.parent.call(this);
+     *         log("B("+this.name+").foo()");
+     *     }
+     * });
+     * </pre>
+     *
+     * @see Function.prototype.addMethods
+     * @param [methods] an associative Object of method names -> functions
+     * @return the new class
+     */
+    subclass: function Function$subclass(methods) {
+        var constructor = (methods && methods.constructor !== Object && methods.constructor) ||
+            (this !== Object &&
+            function Function$DefaultSubclassConstructor() {
+                arguments.callee.parent.call(this);
+            }) ||
+            function Function$ObjectSubclassConstructor() {};
+        constructor.parent = this;
+        constructor.prototype = new this;
+        if (methods) {
+            constructor.addMethods(methods);
+        }
+        constructor.prototype.constructor = constructor;
+        return constructor;
+    },
+    addMethods: function Function$addMethods(methods) {
+        Object.forEach(methods,function(name,method) {
             method.parent = this.parent.prototype[name];
             this.prototype[name] = method;
-		},this);
-	},
-	/**
-	 * Binds a function to an object instance with optional default parameters. Useful for passing around to callbacks.
-	 *
-	 * Example:
-	 * <pre>
-	 *  var objectFoo = object.foo.bind(object);
-	 *  objectFoo(5,4); // same as object.foo(5,4);
-	 *
-	 *  var objectFooFive = object.foo.bind(object,5);
-	 *  objectFoo(4); // same as object.foo(5,4);
-	 * </pre>
-	 *
-	 * @param instance target instance for function
-	 * @return a new function that calls 'this' on
-	 */
-	bind: function Function$bind(instance) {
-		var f = this,
+        },this);
+    },
+    /**
+     * Binds a function to an object instance with optional default parameters. Useful for passing around to callbacks.
+     *
+     * Example:
+     * <pre>
+     *  var objectFoo = object.foo.bind(object);
+     *  objectFoo(5,4); // same as object.foo(5,4);
+     *
+     *  var objectFooFive = object.foo.bind(object,5);
+     *  objectFoo(4); // same as object.foo(5,4);
+     * </pre>
+     *
+     * @param instance target instance for function
+     * @return a new function that calls 'this' on
+     */
+    bind: function Function$bind(instance) {
+        var f = this,
             leftArgs = Array.prototype.slice.call(arguments, 1);
-		return function Function$bindFunc() {
+        return function Function$bindFunc() {
             return f.apply(instance, leftArgs.concat(Array.from(arguments)));
-		};
-	}
+        };
+    }
 });
